2fe91bba6501c63868d54c02c77c6172cfb3f3d3
whygari <whygari4321@gmail.com>
2024-12-06 05:00:30 +0900

    처음 생성

---
+++ backend/src/main/java/com/pretender/myApp/PretenderApplication.java
@@ -0,0 +1,13 @@
+package com.pretender.myApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PretenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PretenderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pretender/myApp/PretenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pretender.myApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PretenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
