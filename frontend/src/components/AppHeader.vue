4a2d2a12e4f943f4886a7eebe3b1db8eaa4de3dc
whygari <whygari4321@gmail.com>
2025-01-10 13:42:58 +0900

    헤더 검색창 구현

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -4,6 +4,18 @@
       Pretender
     </v-app-bar-title>
     <v-spacer></v-spacer>
+    <!-- reference : https://github.com/vuetifyjs/vuetify/blob/6dc52fe22d4e1dbe25e51abb947b11eb2725a21f/packages/vuetify/src/components/VTextField/VTextField.sass-->
+    <div id="search-bar" class="d-flex align-center h-100">
+      <v-text-field
+        class="w-100 mr-3"
+        variant="solo-filled"
+        :rounded="true"
+        append-inner-icon="mdi-magnify"
+        density="compact"
+        v-on:keyup.enter="onEnter"
+        v-model="word"
+      ></v-text-field>
+    </div>
     <div class="mr-3" v-if="isAuthenticated == true">
       <RouterLink  to="/myPage">
         마이페이지
@@ -19,6 +31,16 @@
     </div>
   </v-app-bar>
 </template>
+
+<style>
+  #search-bar {
+    width: var(--search-bar-length)
+  }
+
+  #search-bar .v-input__details {
+    display: none;
+  }
+</style>
 
 <script setup>
   import { ref, onMounted } from 'vue'
@@ -49,4 +71,37 @@
   function savePreviousPage(){
     navigationStore.setPreviousPage(route.fullPath);
   }
+
+  import { useDisplay } from 'vuetify';
+  import { watch } from 'vue'
+
+  const { lgAndUp } = useDisplay();
+
+  const updateSearchBarLength = (isScreenWidthLong) => {
+    if (isScreenWidthLong) {
+      document.documentElement.style.setProperty("--search-bar-length", "25%")
+    } else {
+      document.documentElement.style.setProperty("--search-bar-length", "50%")
+    }
+  }
+
+  onMounted(() => {
+    updateSearchBarLength(lgAndUp.value)
+  })
+
+  watch(() => { return lgAndUp.value }, updateSearchBarLength)
+
+  // 검색 기능
+  import { useRouter, useRoute } from 'vue-router';
+  let word = ref('')
+  const router = useRouter()
+  const onEnter = async () => {
+    router.push({
+            path: '/search',
+            query: {
+              word: word.value
+            }
+          })
+  }
+
 </script>
--- frontend/src/components/search/SearchView.vue
+++ frontend/src/components/search/SearchView.vue
@@ -1,19 +1,21 @@
 <template>
     <AppHeader></AppHeader>
-    <MediaList v-bind:mediaInfo="mediaInfo"/>
+    <!-- mediaListComponentKey가 바뀔떄마다 다시 컴포넌트가 렌더링됨 -->
+    <MediaList :key="mediaListComponentKey" v-bind:mediaInfo="mediaInfo"/>
 </template>
 
 <script setup>
   console.log("rendered...")
   import axios from 'axios'
-  import { ref } from 'vue'
+  import { ref, watch } from 'vue'
   import { useRoute } from 'vue-router';
   import AppHeader from '../AppHeader.vue';
-  let word = useRoute().query.word // 주소창의 url에서 query string을 얻어옴
+  let route = useRoute()
   let mediaInfo = ref({"tv": [], "movie": []}) // 자식 컴포넌트가 length로 접근하기 때문에 에러 방지
+  let mediaListComponentKey = 'initial'
 
   // axios 요청
-  async function getResponse() {
+  async function getResponse(word) {
     let response = await axios.get(
       'http://localhost:8080/api/search',
       {
@@ -28,5 +30,17 @@
     // console.log(mediaInfo.value)
     // console.log("==========mediaInfo=============")
   }
-  getResponse()
+  getResponse(route.query.word)
+
+  // 쿼리 변경 감지
+  watch(
+    () => { return route.query.word },
+    (newWord) => {
+      if (newWord) {
+        mediaInfo.value = {"tv": [], "movie": []}
+        getResponse(newWord)
+        mediaListComponentKey = newWord
+      }
+    },
+  );
 </script>
